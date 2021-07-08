00d1df46dcd655cb0bdfd4ca678447e38ee38b00
유재홍 <yoo.jh@kstm.co.kr>
2021-07-09 01:56:27 +0900

    #2 네트워크 관련 클래스 생성 URL 복수 사용으로 인한 Retrofit instance 싱글톤에서 변경

--- app/src/main/java/com/four/hackerton/line2our/model/network/RetrofitClient.kt
+++ app/src/main/java/com/four/hackerton/line2our/model/network/RetrofitClient.kt
@@ -9,46 +9,53 @@ import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-object RetrofitClient {
-    private var instance : Retrofit? = null
-    private const val TIMEOUT_CONNECT = 60L
-    private const val TIMEOUT_READ = 60L
+class RetrofitClient {
 
-   fun getInstance() : Retrofit{
-       if(instance == null) {
-           instance = Retrofit.Builder()
-               .baseUrl(RemoteConfig.BASE_URL)                              //api 통신을 할 도메인
-               .addConverterFactory(GsonConverterFactory.create())          //reponse 값을 json으로
-               .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
-               .client(createClient())
-               .build()
-       }
-       return instance!!
-   }
+    companion object {
+        private const val TIMEOUT_CONNECT = 60L
+        private const val TIMEOUT_READ = 60L
 
-    private fun createClient() : OkHttpClient {
+        fun <T> create(service : Class<T>) : T {
+            return buildRetrofit(RemoteConfig.BASE_URL).create(service)
+        }
 
-        return OkHttpClient.Builder()
-            .addNetworkInterceptor(
-                HttpLoggingInterceptor().apply {
-                    level = if(RemoteConfig.IS_DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
-                }
-            )
-            .addInterceptor {
-                it.proceed(
-                    it.request().run {
-                        newBuilder().apply {
-                            //token 있다면 header에 토큰 삽입하는 부분
-    //                        if(PreferenceHelper.accessToken.isNotEmpty()) addHeader("Authorization", "Bearer ${PreferenceHelper.accessToken}")
-                            addHeader("Authorization", "KakaoAK ${BuildConfig.KKAO_API_KEY}")
-                            addHeader("Accept", "application/json")
-                            addHeader("Content-Type", "application/json")
-                        }.build()
+        fun <T> create(baseUrl :  String, service : Class<T>) : T {
+            return buildRetrofit(baseUrl).create(service)
+        }
+
+        private fun buildRetrofit(baseUrl : String) : Retrofit {
+            return Retrofit.Builder()
+                .baseUrl(baseUrl)                              //api 통신을 할 도메인
+                .addConverterFactory(GsonConverterFactory.create())          //reponse 값을 json으로
+                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                .client(createClient())
+                .build()
+        }
+
+        private fun createClient() : OkHttpClient {
+
+            return OkHttpClient.Builder()
+                .addNetworkInterceptor(
+                    HttpLoggingInterceptor().apply {
+                        level = if(RemoteConfig.IS_DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
                     }
                 )
-            }
-            .connectTimeout(TIMEOUT_CONNECT, TimeUnit.SECONDS)
-            .readTimeout(TIMEOUT_READ, TimeUnit.SECONDS)
-            .build()
+                .addInterceptor {
+                    it.proceed(
+                        it.request().run {
+                            newBuilder().apply {
+                                //token 있다면 header에 토큰 삽입하는 부분
+                                //                        if(PreferenceHelper.accessToken.isNotEmpty()) addHeader("Authorization", "Bearer ${PreferenceHelper.accessToken}")
+                                addHeader("Authorization", "KakaoAK ${BuildConfig.KKAO_API_KEY}")
+                                addHeader("Accept", "application/json")
+                                addHeader("Content-Type", "application/json")
+                            }.build()
+                        }
+                    )
+                }
+                .connectTimeout(TIMEOUT_CONNECT, TimeUnit.SECONDS)
+                .readTimeout(TIMEOUT_READ, TimeUnit.SECONDS)
+                .build()
+        }
     }
 }
